078179f7c06d04fadfdc904fc16b91be70250190
Ben Spenard <bdspen@gmail.com>
2019-07-11 23:44:02 -0400

    add config file

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
+require('dotenv').config();
 
 // starts the server
 app.listen(port, () => console.log(`server listening on port ${port}`));
