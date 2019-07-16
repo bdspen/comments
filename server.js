1f4ae8d4297a8a7a1bea51845260267c6dea111a
Ben Spenard <bdspen@gmail.com>
2019-07-16 13:24:49 -0400

    serve build folder

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const path = require('path');
 const port = process.env.PORT || 5000;
 require('dotenv').config();
 
@@ -10,6 +11,8 @@ app.use(
     extended: false
   })
 );
+
+app.use(express.static(path.join(__dirname, 'client/build')));
 
 //APP ENDPOINTS
 app.use('/users', require('./controllers/users.controller'));
